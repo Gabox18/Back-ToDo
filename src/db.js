fa91bf9df7d8f6c0c733e4c7c7021b5e76d034fc
Gabriel Ferrer <gefv1891@gmail.com>
2023-07-19 22:48:32 -0400

    comando start/ dotenv

--- src/db.js
+++ src/db.js
@@ -1,11 +1,15 @@
 import mongoose from "mongoose";
+import "dotenv/config";
 
+//require("dotenv").config();
+
+const { URL_DB } = process.env;
 (async () => {
   try {
     mongoose.set("strictQuery", true);
-    const db = await mongoose.connect("mongodb://localhost:27017/mongo-todo");
+    const db = await mongoose.connect(URL_DB);
     console.log("BD connect to", db.connection.name);
   } catch (error) {
-    console.log(error);
+    console.log(error, "----------------->", URL_DB);
   }
 })();

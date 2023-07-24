9d92d03defb63dd9feefff5ea0a62fbcf0644170
Gabriel Ferrer <gefv1891@gmail.com>
2023-07-24 00:21:35 -0400

    proeba de deploy

--- src/app.js
+++ src/app.js
@@ -8,8 +8,8 @@ const app = express();
 // Middlewares
 app.use(morgan("dev"));
 //app.use(urlencoded({extended:false}))
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+app.use(bodyParser.json({ limit: "50mb" }));
 
 app.use((_req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
--- src/db.js
+++ src/db.js
@@ -3,11 +3,11 @@ import "dotenv/config";
 
 //require("dotenv").config();
 
-const { URL_DB } = process.env;
+//const { URL_DB } = process.env;
 (async () => {
   try {
     mongoose.set("strictQuery", true);
-    const db = await mongoose.connect(URL_DB);
+    const db = await mongoose.connect("mongodb+srv://gabox:33181891@cluster0.ykdk2yc.mongodb.net/?retryWrites=true&w=majority");
     console.log("BD connect to", db.connection.name);
   } catch (error) {
     console.log(error, "----------------->", URL_DB);
